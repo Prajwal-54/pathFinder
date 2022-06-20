a285234ef9806151a27e5a648422b3cce2db615a
Prajwal-54V <prajwalmoger40@gmail.com>
2022-06-20 19:53:05 +0530

    Updates

---
+++ precache-manifest.2aa36f71399b9d559f503508263b8c39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2991350594de40a6091f",
+    "url": "/pathFinder/static/css/main.194ffbb3.chunk.css"
+  },
+  {
+    "revision": "2991350594de40a6091f",
+    "url": "/pathFinder/static/js/main.29913505.chunk.js"
+  },
+  {
+    "revision": "edd2e25ed6d341299c11",
+    "url": "/pathFinder/static/js/1.edd2e25e.chunk.js"
+  },
+  {
+    "revision": "1ee68610300524b8d54d",
+    "url": "/pathFinder/static/js/runtime~main.1ee68610.js"
+  },
+  {
+    "revision": "daf1d02eb8a007d1ae86cbfcdb5b32c0",
+    "url": "/pathFinder/index.html"
+  }
+];
